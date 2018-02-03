53d3024a7ecc052ed46413330db8e03682a7ed10
Wilfried Klaas <w.klaas@gmx.de>
2018-02-03 10:14:17 +0100

    generating the service classes

---
+++ schematic/src/generated/java/de/mcs/microservice/schematic/MyServiceConfig.java
@@ -0,0 +1,12 @@
+package de.mcs.microservice.schematic;
+/**
+ * MyServiceConfig
+ * this is the configuration class of the service
+ * Copyright (c) MCS 2018
+ */
+
+import de.mcs.microservice.application.AppServiceConfig;
+
+public class MyServiceConfig extends AppServiceConfig {
+
+}
